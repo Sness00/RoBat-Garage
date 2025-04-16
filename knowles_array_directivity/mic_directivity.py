861259a8fa51ec8282f6bc81934610de8bfc53e3
Sness00 <gabriele.baroli@mail.polimi.it>
2025-04-16 10:31:16 +0200

    security checks

--- knowles_array_directivity/mic_directivity.py
+++ knowles_array_directivity/mic_directivity.py
@@ -6,6 +6,8 @@ import os
 
 if __name__ == '__main__':
     os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    PREPROCESS = False
+    IN_BANDS = False
     DIR = './recordings/'
     CUT_DIR = 'mic_'
     series = 2
@@ -17,23 +19,23 @@ if __name__ == '__main__':
     sweep *= signal.windows.tukey(sweep.size, 0.2)
     sig = 0.8*sweep
     
-
-    # audio_files = [f for f in os.listdir(DIR) if f.endswith('.wav')]
-    # for i in np.arange(8):
-    #     save_dir = os.path.join('series_' + str(series), CUT_DIR + str(i+1))
-    #     print(save_dir)
-    #     if not os.path.exists(save_dir):
-    #         os.makedirs(save_dir)       
-    #     for f in audio_files:
-    #         x = sf.read(os.path.join(DIR, f))[0]
-    #         x = x[:, i]            
-    #         xcorr = signal.correlate(x, sig, mode='same')
-    #         xcorr_rolled = np.roll(xcorr, -len(sig)//2)
-    #         envelope = np.abs(signal.hilbert(xcorr_rolled))
-    #         idxs = signal.find_peaks(envelope, prominence=0.5, distance=int(30e-3*fs))[0]
-    #         for n, idx in enumerate(idxs[5*(series-1):5*series]):
-    #             x_trimmed = x[idx-384:idx + int(durn*fs) + 384]
-    #             sf.write(save_dir + '/' + f[0:3] + 'deg_' + str(n+1) + '.wav', x_trimmed, int(fs))
+    if PREPROCESS:
+        audio_files = [f for f in os.listdir(DIR) if f.endswith('.wav')]
+        for i in np.arange(8):
+            save_dir = os.path.join('series_' + str(series), CUT_DIR + str(i+1))
+            print(save_dir)
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)       
+            for f in audio_files:
+                x = sf.read(os.path.join(DIR, f))[0]
+                x = x[:, i]            
+                xcorr = signal.correlate(x, sig, mode='same')
+                xcorr_rolled = np.roll(xcorr, -len(sig)//2)
+                envelope = np.abs(signal.hilbert(xcorr_rolled))
+                idxs = signal.find_peaks(envelope, prominence=0.5, distance=int(30e-3*fs))[0]
+                for n, idx in enumerate(idxs[5*(series-1):5*series]):
+                    x_trimmed = x[idx-384:idx + int(durn*fs) + 384]
+                    sf.write(save_dir + '/' + f[0:3] + 'deg_' + str(n+1) + '.wav', x_trimmed, int(fs))
 
     NFFT = 2048
     theta = np.linspace(0, 350, 36)
@@ -54,8 +56,7 @@ if __name__ == '__main__':
             Channels_uni = Channels[:, 0:NFFT//2]
             freqs = fft.fftfreq(NFFT, 1 / fs)
             freqs = freqs[0:NFFT//2]
-            R = 1
-            radiance = 4 * np.pi * R * np.abs(Channels_uni)
+            radiance = np.abs(Channels_uni)**2
             radiances.append(radiance)
 
         radiances = np.array(radiances)
@@ -76,74 +77,75 @@ if __name__ == '__main__':
     ax.set_xticks(np.linspace(0, 2 * np.pi, 18, endpoint=False))
     ax.set_ylabel("dB")
     # less radial ticks
-    ax.set_yticks(np.linspace(-40, 0, 5))
+    ax.set_yticks(np.linspace(-40, 0, 11))
     ax.set_rlabel_position(-90)
     ax.set_title('Microphones Directivity Patterns')
     ax.legend(loc="upper right", bbox_to_anchor=(1.1, 1.1))
     plt.tight_layout()
     plt.show()
 
-    central_freq = np.array([20e3, 30e3, 40e3, 50e3, 60e3, 70e3, 80e3, 90e3])
-    BW = 2e3
+    if IN_BANDS:
+        central_freq = np.array([20e3, 30e3, 40e3, 50e3, 60e3, 70e3, 80e3, 90e3])
+        BW = 2e3
 
-    linestyles = ["-", "--", "-.", ":"]
+        linestyles = ["-", "--", "-.", ":"]
 
-    for k in np.arange(8):
-        fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={"projection": "polar"})
-        plt.suptitle("Microphone " + str(k+1) + " Directivity Pattern")
-        i = 3
-        for fc in central_freq[0:4]:
-            rad_patt = np.mean(
-                mean_radiances[k, :, (freqs < fc + BW) & (freqs > fc - BW)], axis=0
-            )
-            rad_patt_norm = rad_patt / np.max(rad_patt)
-            rad_patt_norm_dB = 20 * np.log10(rad_patt_norm)
-            rad_patt_norm_dB = np.append(rad_patt_norm_dB, rad_patt_norm_dB[0])
-            ax1.plot(
-                np.deg2rad(theta),
-                rad_patt_norm_dB,
-                label=str(fc)[0:2] + " [kHz]",
-                linestyle=linestyles[i]
+        for k in np.arange(8):
+            fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={"projection": "polar"})
+            plt.suptitle("Microphone " + str(k+1) + " Directivity Pattern")
+            i = 3
+            for fc in central_freq[0:4]:
+                rad_patt = np.mean(
+                    mean_radiances[k, :, (freqs < fc + BW) & (freqs > fc - BW)], axis=0
                 )
-            i -= 1
-        ax1.legend(loc="upper right", bbox_to_anchor=(1.1, 1.1))
-        # offset polar axes by -90 degrees
-        ax1.set_theta_offset(np.pi / 2)
-        # set theta direction to clockwise
-        ax1.set_theta_direction(-1)
-        # more theta ticks
-        ax1.set_xticks(np.linspace(0, 2 * np.pi, 18, endpoint=False))
-        # less radial ticks
-        ax1.set_yticks(np.linspace(-40, 0, 5))
-        ax1.set_rlabel_position(100)
-        ax1.set_rlabel_position(-90)
+                rad_patt_norm = rad_patt / np.max(rad_patt)
+                rad_patt_norm_dB = 20 * np.log10(rad_patt_norm)
+                rad_patt_norm_dB = np.append(rad_patt_norm_dB, rad_patt_norm_dB[0])
+                ax1.plot(
+                    np.deg2rad(theta),
+                    rad_patt_norm_dB,
+                    label=str(fc)[0:2] + " [kHz]",
+                    linestyle=linestyles[i]
+                    )
+                i -= 1
+            ax1.legend(loc="upper right", bbox_to_anchor=(1.1, 1.1))
+            # offset polar axes by -90 degrees
+            ax1.set_theta_offset(np.pi / 2)
+            # set theta direction to clockwise
+            ax1.set_theta_direction(-1)
+            # more theta ticks
+            ax1.set_xticks(np.linspace(0, 2 * np.pi, 18, endpoint=False))
+            # less radial ticks
+            ax1.set_yticks(np.linspace(-40, 0, 5))
+            ax1.set_rlabel_position(100)
+            ax1.set_rlabel_position(-90)
 
-        i = 3
-        for fc in central_freq[4:8]:
-            rad_patt = np.mean(
-                mean_radiances[k, :, (freqs < fc + BW) & (freqs > fc - BW)], axis=0
-            )
-            rad_patt_norm = rad_patt / np.max(rad_patt)
-            rad_patt_norm_dB = 20 * np.log10(rad_patt_norm)
-            rad_patt_norm_dB = np.append(rad_patt_norm_dB, rad_patt_norm_dB[0])
-            ax2.plot(
-                np.deg2rad(theta),
-                rad_patt_norm_dB,
-                label=str(fc)[0:2] + " [kHz]",
-                linestyle=linestyles[i]
-            )
-            i -= 1
-        ax2.legend(loc="upper right", bbox_to_anchor=(1.1, 1.1))
-        # offset polar axes by -90 degrees
-        ax2.set_theta_offset(np.pi / 2)
-        # set theta direction to clockwise
-        ax2.set_theta_direction(-1)
-        # more theta ticks
-        ax2.set_xticks(np.linspace(0, 2 * np.pi, 18, endpoint=False))
-        # less radial ticks
-        ax2.set_yticks(np.linspace(-40, 0, 5))
-        ax2.set_rlabel_position(100)
-        ax2.set_rlabel_position(-90)
+            i = 3
+            for fc in central_freq[4:8]:
+                rad_patt = np.mean(
+                    mean_radiances[k, :, (freqs < fc + BW) & (freqs > fc - BW)], axis=0
+                )
+                rad_patt_norm = rad_patt / np.max(rad_patt)
+                rad_patt_norm_dB = 20 * np.log10(rad_patt_norm)
+                rad_patt_norm_dB = np.append(rad_patt_norm_dB, rad_patt_norm_dB[0])
+                ax2.plot(
+                    np.deg2rad(theta),
+                    rad_patt_norm_dB,
+                    label=str(fc)[0:2] + " [kHz]",
+                    linestyle=linestyles[i]
+                )
+                i -= 1
+            ax2.legend(loc="upper right", bbox_to_anchor=(1.1, 1.1))
+            # offset polar axes by -90 degrees
+            ax2.set_theta_offset(np.pi / 2)
+            # set theta direction to clockwise
+            ax2.set_theta_direction(-1)
+            # more theta ticks
+            ax2.set_xticks(np.linspace(0, 2 * np.pi, 18, endpoint=False))
+            # less radial ticks
+            ax2.set_yticks(np.linspace(-40, 0, 5))
+            ax2.set_rlabel_position(100)
+            ax2.set_rlabel_position(-90)
 
-        plt.tight_layout()
-        plt.show()
+            plt.tight_layout()
+            plt.show()
