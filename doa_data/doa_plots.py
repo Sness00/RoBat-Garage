affd7cb4dea66402ad5609d62e3ce6e13fa1fecc
Sness00 <gabriele.baroli@mail.polimi.it>
2025-04-30 16:33:16 +0200

    doa plotter

---
+++ doa_data/doa_plots.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    plt.rcParams['text.usetex'] = False
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    # Set the path to the directory containing the .npy files
+    data_dir = './pseudospectra/'
+    
+    # Load the data from the .npy files
+    p_dB = np.load(data_dir  + 'das_20250429_18-00-55_-60.npy')
+    theta = np.linspace(-90, 90, p_dB.shape[0])    
+    theta_bar = theta[np.argmax(p_dB)]
+    
+    # Create a scatter plot of the DOA data
+    fig, ax2 = plt.subplots(subplot_kw={'projection': 'polar'})
+    ax2.plot(np.deg2rad(theta), p_dB)
+    ax2.axvline(np.deg2rad(theta_bar), color='g', linestyle='dashed')
+    ax2.set_title('Pseudospectrum')
+    ax2.set_theta_offset(np.pi/2)
+    ax2.set_xlim(-np.pi/2, np.pi/2)
+    ax2.set_xticks(np.deg2rad([-80, -60, -40, -20, 0, 20, 40, 60, 80]))    
+    ax2.grid(True)
+    plt.show()
