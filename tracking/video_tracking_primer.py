f14aefbbc0e53a95ff456609f09b668e36ef51ab
Sness00 <gabriele.baroli@mail.polimi.it>
2025-05-14 20:38:22 +0200

    audio and video are synced

--- tracking/video_tracking_primer.py
+++ tracking/video_tracking_primer.py
@@ -5,6 +5,16 @@ import traceback
 import yaml
 import numpy as np
 import pyautogui as pag
+import sys
+import io
+import ffmpeg
+import soundfile as sf
+import librosa
+from sonar import sonar
+from scipy import signal
+from das_v2 import das_filter
+from capon import capon_method
+from matplotlib import pyplot as plt
 
 # Cross product in 2D
 def cross2d(x, y):
@@ -84,13 +94,46 @@ def shift_toward_point(points, p1, shift_cm, px_per_cm):
 
     return np.array(coordinates), np.array(distances)
 
+def insert_between_large_diffs(arr):
+    result = []
+
+    for i in range(len(arr) - 1):
+        result.append(arr[i])
+        diff = arr[i+1] - arr[i]
+
+        if abs(diff) > 10:
+            # Insert two evenly spaced values between arr[i] and arr[i+1]
+            step = diff / 3
+            result.append(arr[i] + step)
+            result.append(arr[i] + 2 * step)
+
+    result.append(arr[-1])  # Add the last element
+    return np.array(result)
+
+def pow_two_pad_and_window(vec, show=False):
+    window = signal.windows.tukey(len(vec), alpha=0.3)
+    windowed_vec = vec * window
+    padded_windowed_vec = np.pad(windowed_vec, (0, 2**int(np.ceil(np.log2(len(windowed_vec)))) - len(windowed_vec)))
+    if show:
+        dur = len(padded_windowed_vec) / fs
+        t = np.linspace(0, dur, len(padded_windowed_vec))
+        plt.figure()
+        plt.plot(t, padded_windowed_vec)
+        plt.show()
+    return padded_windowed_vec/max(padded_windowed_vec)*0.8
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+camera_path = './videos/GX010518.mp4'
+robot_path = './audio/20250514_17-07-06.wav'
+offsets = np.load('./offsets/20250514_17-07-06_offsets.npy')
+video_fps = 60
 screen_width, screen_height = pag.size()
 robot_id = 0
 arena_w = 1
 arena_l = 1.7
 # Load video file
-video_path = './videos/GX010518.mp4'
+video_path = camera_path
 cap = cv2.VideoCapture(video_path)
 
 # Camera calibration parameters
@@ -111,18 +154,107 @@ object_points.append(np.array([[-marker_length/2, marker_length/2, 0],
 object_points = np.array(object_points, dtype='float32')
 
 # Loop through the video
-frame_count = 0
-trajectory = np.zeros((0, 2), dtype=np.float32)
 try:
+    robot_audio, fs = sf.read(robot_path)
+    robot_audio = robot_audio[:, 0]
+    print( 'Robot audio duration: %.1f [s]' % (len(robot_audio)/fs))
+    # Run ffmpeg to extract audio and pipe as WAV
+    out, _ = (
+        ffmpeg
+        .input(camera_path)
+        .output('pipe:', format='wav', acodec='pcm_s16le')
+        .run(capture_stdout=True, capture_stderr=True)
+    )
+
+    # Load audio from bytes using soundfile
+    camera_audio, sr = librosa.load(io.BytesIO(out), sr=fs, mono=True)
+    print( 'Camera audio duration: %.1f [s]' % (len(camera_audio)/fs))
+
+    xcorr = np.roll(signal.correlate(camera_audio, robot_audio, mode='same'), -len(robot_audio) // 2)
+    index = np.argmax(np.abs(xcorr))
+    start_frame = int(index / sr * video_fps)
+    print('Start frame: %d' % start_frame)
+    video_frames = np.astype((offsets[:, 0]) / fs * video_fps + start_frame, np.int32)
+    interp_video_frames = np.astype(insert_between_large_diffs(video_frames), np.int32)
+    print(video_frames.shape, interp_video_frames.shape)
+
+    fs = 176400
+    dur = 3e-3
+    hi_freq = 60e3
+    low_freq = 20e3
+    output_threshold = -50 # [dB]
+    distance_threshold = 20 # [cm]
+
+    METHOD = 'das' # 'das', 'capon'
+    if METHOD == 'das':
+        spatial_filter = das_filter
+    elif METHOD == 'capon':
+        spatial_filter = capon_method
+
+    t_tone = np.linspace(0, dur, int(fs*dur))
+    chirp = signal.chirp(t_tone, hi_freq, t_tone[-1], low_freq)    
+    sig = pow_two_pad_and_window(chirp)
+
+    C_AIR = 343
+    min_distance = 10e-2
+    discarded_samples = int(np.floor(((min_distance + 2.5e-2)*2)/C_AIR*fs))
+    max_distance = 1
+    max_index = int(np.floor(((max_distance + 2.5e-2)*2)/C_AIR*fs))
+
+    def update(frame):
+        # print(curr_end/fs)
+        audio_data, _ = sf.read(robot_path, start=offsets[frame, 0], frames=offsets[frame, 1])
+        # video_frame = int(offsets[frame, 0] / fs * video_fps) + start_frame
+        # print('Video frame: %d' % video_frame)  
+        dB_rms = 20*np.log10(np.mean(np.std(audio_data, axis=0)))    
+        if dB_rms > output_threshold:
+            filtered_signals = signal.correlate(audio_data, np.reshape(sig, (-1, 1)), 'same', method='fft')
+            roll_filt_sigs = np.roll(filtered_signals, -len(sig)//2, axis=0)
+            
+            try:
+                distance, direct_path, obst_echo = sonar(roll_filt_sigs, discarded_samples, max_index, fs)
+                distance = distance*100 # [m] to [cm]
+                theta, p = spatial_filter(
+                                            roll_filt_sigs[obst_echo - int(5e-4*fs):obst_echo + int(5e-4*fs)], 
+                                            fs=fs, nch=roll_filt_sigs.shape[1], d=2.70e-3, 
+                                            bw=(low_freq, hi_freq)
+                                        )
+                p_dB = 10*np.log10(p)
+                
+                if direct_path != obst_echo:
+                    doa_index = np.argmax(p_dB)
+                    theta_hat = theta[doa_index]
+                    if distance > 0:
+                        # print('\nDistance: %.1f [cm] | DoA: %.2f [deg]' % (distance, theta_hat))            
+                        return distance, theta_hat
+                    else: return 0, 0
+                else: return 0, 0
+            except ValueError:
+                print('\nNo valid distance or DoA')
+                return 0, 0
+        else:
+            return 0, 0
+
+except ffmpeg.Error as e:
+    print('ffmpeg error:', e.stderr.decode(), file=sys.stderr)
+    sys.exit(1)
+try:
+    frame_count = 0
+    trajectory = np.zeros((0, 2), dtype=np.float32)
+    counter = 0
+    true_counter = 0
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
+        # print(interp_video_frames[counter], frame_count)
         # print(frame_count)
-        if frame_count % 5 == 0:
-
+        if frame_count == interp_video_frames[counter]:
+            counter += 1
+            if frame_count in video_frames: 
+                distance, doa = update(true_counter)
+                true_counter += 1
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
             # Detect markers
             detector = aruco.ArucoDetector(aruco_dict, parameters)
             corners, ids, _ = detector.detectMarkers(gray)
@@ -189,8 +321,10 @@ try:
                                     break
                             # print(np.rad2deg(angle))
                             if (found_nearest and angle <= np.pi/2):
-                                # print distance and angle
-                                print("Distance: %.1f [cm], Angle: %.1f [deg]" % (sd, np.rad2deg(angle)))
+                                # print distance and angle                                
+                                if distance != 0:
+                                    print("Distance: %.1f [cm], Angle: %.1f [deg]" % (distance, doa))
+                                    print("GT Distance: %.1f [cm], GT Angle: %.1f [deg]\n" % (sd, np.rad2deg(angle)))
                                 cv2.line(frame, mic_positions, closest_obstacle.astype(int), (255, 51, 20), 2)
                                 # print angle and id of nearest obstacle
                                 # cv2.putText(frame, str(obst_ids[np.where(distances == sd)][0]), closest_obstacle.astype(int), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
