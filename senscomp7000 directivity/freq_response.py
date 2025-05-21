93727aea4ee7f37459cc418b4d9814de68a35f60
Sness00 <gabriele.baroli@mail.polimi.it>
2025-05-21 15:33:14 +0200

    transducer frequency response

---
+++ senscomp7000 directivity/freq_response.py
@@ -0,0 +1,77 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import soundfile as sf
+from scipy import fftpack
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+rec_dir = './gras_20250416/'
+sweeps_dir = 'sweeps_1/'
+calib_dir = 'calib/'
+
+signals = os.listdir(rec_dir + sweeps_dir)
+# Filter out non-WAV files
+signals = [f for f in signals if f.endswith('.wav')]
+
+freq_response = []
+for signal in signals:
+    # Load the signal
+    signal_path = os.path.join(rec_dir, sweeps_dir, signal)
+    data, fs = sf.read(signal_path)
+    print(20*np.log10(np.sqrt(np.mean(data**2))))
+    # print(fs)    
+    # Perform FFT
+    # Zero padding to the next power of 2
+    N = len(data)
+    N = 2**np.ceil(np.log2(N)).astype(int)
+    data = np.pad(data, (0, N - len(data)), 'constant')
+    T = 1.0 / fs
+    yf = fftpack.fft(data)
+    xf = np.fft.fftfreq(N, T)[:N//2]
+    # append the frequency response
+    freq_response.append(20*np.log10(np.sqrt((2/N * np.abs(yf[0:N//2])**2))))
+
+# Convert to numpy array for easier manipulation
+freq_response = np.array(freq_response)
+# Calculate mean
+mean_freq_response_dB = np.mean(freq_response, axis=0)
+
+# Load calibration data
+calib_signal = sf.read(os.path.join(rec_dir, calib_dir, 'calibration_tone.wav'))[0]
+print(20*np.log10(np.sqrt(np.mean(calib_signal**2))))
+# N = len(calib_signal)
+# N = 2**np.ceil(np.log2(N)).astype(int)
+# print(N)
+# data = np.pad(calib_signal, (0, N - len(calib_signal)), 'constant')
+# T = 1.0 / fs
+# yf = fftpack.fft(data)
+# xfs = np.fft.fftfreq(N, T)[:N//2]
+
+dB_SPL = 94
+# Compute the rms for calibration signal
+dB_SPL_to_rms = dB_SPL - 20*np.log10(np.sqrt(np.mean(calib_signal**2))) 
+
+# append the frequency response
+# calib_freq_response = 20*np.log10(2.0/N * np.abs(yf[0:N//2])) + dB_SPL - dB_SPL_to_rms
+# Convert the mean frequency response to dB SPL
+mean_freq_response_dB = mean_freq_response_dB + dB_SPL_to_rms
+
+# plot the frequency response of the calibration signal
+plt.figure(figsize=(12, 8))
+plt.plot(xf, mean_freq_response_dB, color='black', alpha=0.5)
+plt.title('Sweep Signal Frequency Response')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('SPL [dB] ref 1[kHz] @ 94 [dB SPL]')
+plt.grid()
+plt.xlim(15000, 86000)
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+    
+    
